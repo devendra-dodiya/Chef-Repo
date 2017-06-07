a50a596a8baf572dbf72d28474e4d03cfec5dbcd
Devops <dodiya_devendra@yahoo.co.in>
2017-06-07 14:00:25 +0000

    Change for recepies

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook:: apache
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+if node['platform_family'] == "rhel"
+        package = "httpd"
+elsif node['platform_family'] == "debian"
+        package = "apache2"
+end
+package 'apache2' do
+        package_name package
+        action :install
+end
+if node['platform_family'] == "rhel"
+        service = "httpd"
+elsif node['platform_family'] == "debian"
+        service = "apache2"
+end
+service 'apache' do
+        service_name service
+        action [:enable, :start]
+end
+
---
+++ cookbooks/apache/recipes/website.rb
@@ -0,0 +1,4 @@
+file 'default index-html' do
+	path '/var/www/html/index.html'
+	content 'Hello REHL' 
+end
